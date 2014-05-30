c1803a3a515c822dac9219fb8a744872bb00871e
Toofifty <amatho250@gmail.com>
2014-05-30 23:17:54 +1000

    Added hashlinking

--- js/jquery.coda-slider-3.0.js
+++ js/jquery.coda-slider-3.0.js
@@ -393,7 +393,7 @@ if ( typeof Object.create !== 'function' ) {
 		dynamicTabsPosition: "top",
 		externalTriggerSelector: "a.xtrig", //shouldnt need any more
 		firstPanelToLoad: 1,
-		hashLinking: false,
+		hashLinking: true,
 		panelTitleSelector: "h2.title",
 		slideEaseDuration: 1500,
 		slideEaseFunction: "easeInOutExpo"
