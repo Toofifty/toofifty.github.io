580285746f892679aa80ba746e0485f28ec6365a
Alex Matheson <s3542341@student.rmit.edu.au>
2016-06-16 22:06:22 +1000

    Updated buttons to fill buttons, made project animations better

--- js/main.js
+++ js/main.js
@@ -1,35 +1,67 @@
-$(window).scroll(function (e) {
-   var anchor = $("#about").offset().top;
-   
-   if ($(this).scrollTop() > anchor) {
-       $('.nav').css({
-          'background': 'rgba(0, 0, 0, 0.6)'
-       });
-       $('.nav > a').css({
-          'padding-top': '5px',
-          'padding-bottom': '5px',
-          'margin-top': '5px',
-          'margin-bottom': '5px'
-       });
-       $('h1').css({
-          'margin-top': '3px',
-          'margin-bottom': '3px'
-       });
-   } else {
-       $('.nav').css({
-          'background': 'rgba(0, 0, 0, 0)'
-       });
-       $('.nav > a').css({
-          'padding-top': '10px',
-          'padding-bottom': '10px',
-          'margin-top': '10px',
-          'margin-bottom': '10px'
-       });
-       $('h1').css({
-          'margin-top': '13px',
-          'margin-bottom': '13px'
-       });
-   }
+$(document).ready(function() {
+
+  var sel = "#scr-home";
+
+  $(document).scroll(function (e) {
+
+    var scroll = $(this).scrollTop();
+    var anchor = $("#about").offset().top;
+
+    // change navbar
+
+    if (scroll > anchor) {
+      $('.nav').css({
+        'background': '#222',
+        'box-shadow': '0px 2px 10px rgba(0,0,0,0.3)'
+      });
+      $('.nav > a').css({
+        'padding-top': '5px',
+        'padding-bottom': '5px',
+        'margin-top': '5px',
+        'margin-bottom': '5px'
+      });
+      $('h1').css({
+        'margin-top': '3px',
+        'margin-bottom': '3px'
+      });
+    } else {
+      $('.nav').css({
+        'background': 'rgba(0, 0, 0, 0)',
+        'box-shadow': 'none'
+      });
+      $('.nav > a').css({
+        'padding-top': '10px',
+        'padding-bottom': '10px',
+        'margin-top': '10px',
+        'margin-bottom': '10px'
+      });
+      $('h1').css({
+        'margin-top': '13px',
+        'margin-bottom': '13px'
+      });
+    }
+
+    var set = function(id) {
+      if (sel == id) return;
+      $("[id^='scr-']").removeClass("btn-sel");
+      $(id).addClass("btn-sel");
+      sel = id;
+    }
+
+    // set selected page
+
+    if (scroll < $("#about").offset().top) {
+      set("#scr-home");
+    } else if (scroll < $("#portfolio").offset().top) {
+      set("#scr-about");
+    } else if (scroll < $("#contact").offset().top) {
+      set("#scr-portfolio");
+    } else {
+      set("#scr-contact");
+    }
+
+  });
+
 });
 
 function scrollTo(element) {
