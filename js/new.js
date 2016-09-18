d46bcf56b01530a826010fe8cdbda6fd3a310f99
Toofifty <amatho250@gmail.com>
2016-09-18 17:52:47 +1000

    Tweaks and code cleaning in site rework 'new'

--- js/new.js
+++ js/new.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+	var scroll_colour = false;
+
 	var hue = 0;
 
 	var hsl = function() {
@@ -71,12 +73,95 @@ $(document).ready(function() {
 
 	setTimeout(scroll_title, 2500);
 
-	var test_hue = function() {
-		hue += 1;
-		$(".color-fill").css("background", hsl());
-		setTimeout(test_hue, 200);
+	var sel = "#scr-home";
+	var perc = 0;
+	var anchor = $("#about").offset().top;
+	var document_height = $(document).innerHeight();
+
+	$(document).scroll(function() {
+		// 
+		var scroll = $(this).scrollTop();
+
+		perc = scroll / document_height;
+
+		// change navbar
+		if (scroll > anchor && sel == "#scr-home") {
+
+			$(".nav > a").css({
+				"padding-top": "5px",
+				"padding-bottom": "5px",
+				"margin-top": "5px",
+				"margin-bottom": "5px"
+			});
+
+			$(".logo").css({
+				"margin-top": "3px",
+				"margin-bottom": "3px"
+			})
+
+		} else if (scroll < anchor && sel != "#scr-home") {
+
+			$(".nav > a").css({
+				"padding-top": "10px",
+				"padding-bottom": "10px",
+				"margin-top": "10px",
+				"margin-bottom": "10px"
+			});
+
+			$(".logo").css({
+				"margin-top": "13px",
+				"margin-bottom": "13px"
+			})
+
+		}
+
+		var set = function(id) {
+			if (sel == id) return;
+			$(sel).removeClass("btn-sel");
+			$(id).addClass("btn-sel");
+			sel = id;
+			console.log(sel);
+		}
+
+		if (scroll < $("#about").offset().top) 
+			set("#scr-home");
+		else if (scroll < $("#portfolio").offset().top)
+			set("#scr-about");
+		else if (scroll < $("#contact").offset().top)
+			set("#scr-portfolio");
+		else
+			set("#scr-contact");
+
+		// update fill colour
+		if (!scroll_colour) return;
+
+		hue = perc * 255;
+		$(".ko-bg").css("background", hsl());
+	});
+
+	if (!scroll_colour) {
+		var update_colour = function() {
+			hue += 1;
+			hue %= 255;
+			$(".ko-bg").css("background", hsl());
+			setTimeout(update_colour, 200);
+		}
+
+		setTimeout(update_colour, 200);
 	}
 
-	//setTimeout(test_hue, 200);
+	$(".skill").each(function() {
+		var width = $(this).children(".skill-progress").innerWidth() / $(this).innerWidth();
+		//$(this).css("background", "hsl(" + (width * 128) + ", 65%, 35%)");
+		$(this).children(".skill-progress").css("background", "hsl(" + (width * 128) + ", 50%, 50%)");
+	});
 
 });
+
+function scrollTo(element) {
+    var position = 0;
+    if (element != 0) {
+        position = $(element).offset().top;
+    }
+    $("html, body").animate({ scrollTop: position }, "slow");
+}
